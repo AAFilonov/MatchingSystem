f6b4569e823e0464fa316854765896b590c7ff0b
Ivan Semenov <ivan32s2015@gmail.com>
2022-05-16 01:30:24 +0300

    MatchingSystem.Data обновлено. Feature обновлено

---
+++ MatchingSystem.Data/Feature/Log/LogTable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MatchingSystem.Data.Feature.Log;
+
+public class LogTable : IEntityTypeConfiguration<Model.Log>
+{
+    public void Configure(EntityTypeBuilder<Model.Log> builder)
+    {
+        builder.HasNoKey();
+        builder.ToTable("Log");
+        builder.Property(e => e.Endpoint).HasColumnType("text");
+        builder.Property(e => e.Id).HasDefaultValueSql("(newid())");
+        builder.Property(e => e.Request).HasColumnType("text");
+    }
+}
