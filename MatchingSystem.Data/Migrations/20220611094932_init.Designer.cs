95a305cd1181806639c6019f2e88963940a1c2ad
Ivan Semenov <semenov_ia@itprocomp.ru>
2022-06-11 12:58:24 +0300

    FIX-Data

--- MatchingSystem.Data/Migrations/20220611094932_init.Designer.cs
+++ MatchingSystem.Data/Migrations/20220611094932_init.Designer.cs
@@ -4,6 +4,7 @@ using MatchingSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MatchingSystem.Data.Migrations
 {
     [DbContext(typeof(DiplomaMatchingContext))]
-    partial class DiplomaMatchingContextModelSnapshot : ModelSnapshot
+    [Migration("20220611094932_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
